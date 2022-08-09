60d5d7c43b0e2daca57be4357e886dd0b5c08629
milu <me@milu.run>
2022-08-09 14:26:03 +0800

    add google analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,6 +49,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        googleAnalytics: {
+          trackingID: 'G-42GWC5FCG2',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
